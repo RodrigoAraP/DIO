c4ad0af35dcaeff8a41349ceda7312a20e2c4a1a
Rodrigo Araújo Passos <araujo.rodrigo.passos@gmail.com>
2025-10-14 17:14:47 -0300

    Unificação do .gitignore na raiz do projeto

---
+++ Formacao .NET Developer/Propriedades, Metodos e Contrutores/Models/Curso.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Propriedades__Metodos_e_Contrutores.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadeDeAlunosMatriculados() => Alunos.Count;
+
+        public bool RemoverAluno(Pessoa aluno) => Alunos.Remove(aluno);
+
+        public void ListarAlunos()
+        {
+            Console.WriteLine($"Alunos do curso: {Nome}");
+            foreach (Pessoa aluno in Alunos)
+            {
+                Console.WriteLine(aluno.NomeCompleto);
+            }
+        }
+    }
+}
---
+++ Formacao .NET Developer/Propriedades, Metodos e Contrutores/Program.cs
@@ -0,0 +1,12 @@
+﻿using Propriedades__Metodos_e_Contrutores.Models;
+
+Pessoa p1 = new("Rodrigo", "Araujo");
+Pessoa p2 = new(nome: "Ana Clara", sobrenome: "Souza");
+
+Curso cursoDeIngles = new();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
